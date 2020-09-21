9661cc32acff198afe5299c38bc0ab10532030f6
Donething <donething.net@gmail.com>
2020-09-22 02:30:02 +0800

    1. 增加开机启动

--- app/src/main/java/net/donething/android/wifiadb/BootReceiver.kt
+++ app/src/main/java/net/donething/android/wifiadb/BootReceiver.kt
@@ -3,14 +3,14 @@ package net.donething.android.wifiadb
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.Intent.ACTION_BOOT_COMPLETED
 import android.content.SharedPreferences
+import android.widget.Toast
 
 class BootReceiver : BroadcastReceiver() {
     private var prefs: SharedPreferences? = null
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             prefs =
                 prefs ?: context.getSharedPreferences(MainActivity.PREFS_NAME, Context.MODE_PRIVATE)
             if (prefs?.getBoolean(MainActivity.SW_ADB, false) == true) {
